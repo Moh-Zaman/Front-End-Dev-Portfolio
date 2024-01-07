44f44340909a3bc47bb53949c566fa14710fe88a
Moh-Zaman <145913815+Moh-Zaman@users.noreply.github.com>
2024-01-07 20:22:14 +0000

    Nav Bar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
-import './App.css'
+import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
+
+function App() {
+    return (
+        <div className={styles.App}>
+            <Navbar />
+        </div>
+    )
+}
 
 
 export default App
