b856f97edc0ea6da016c04965b151a6ecec57177
Moh-Zaman <145913815+Moh-Zaman@users.noreply.github.com>
2024-01-07 22:47:00 +0000

    Added in contact section, need to change assets now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
-// import { Contact } from "./components/Contact/Contact";
+import { Contact } from "./components/Contact/Contact";
 import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
@@ -14,7 +14,7 @@ function App() {
       <About />
       <Experience />
       <Projects />
-      {/* <Contact /> */}
+      <Contact />
     </div>
   );
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 
-const Contact = () => {
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default Contact
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contacts</h2>
+        <p>Feel free to contact me!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:Moh.Zaman@hotmail.com">Moh.Zaman@hotmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/moh-zaman-460733174/">linkedin.com/Moh-Zaman</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Moh-Zaman">github.com/Moh-Zaman</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
