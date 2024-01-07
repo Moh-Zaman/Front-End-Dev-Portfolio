c2eeab729129aa99cc8dde5d6649f76646cf0b16
Moh-Zaman <145913815+Moh-Zaman@users.noreply.github.com>
2024-01-07 20:54:12 +0000

    Hero Banner Completed, Free asset will be replaced

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Hero } from "./components/Hero/Hero";
 
 function App() {
     return (
         <div className={styles.App}>
             <Navbar />
+            <Hero />
         </div>
     )
 }
-
 
 export default App
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Moh</h1>
+        <p className={styles.description}>
+          I'm a front-end developer experienced in react and nodejs. Reach out
+          if you'd like to communicate with me.
+        </p>
+        <a href="mailto:Moh.Zaman@hotmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
+
+export default Hero;
